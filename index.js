3aa09174afb23b42090f5f5959667a9a1217b11b
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-12 11:51:06 -0500

    met all specs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+const saturdayFun = (thing = 'roller-skate') =>
+  `This Saturday, I want to ${thing}!`;
+
+const mondayWork = (thing = 'go to the office') =>
+  `This Monday, I will ${thing}.`;
+
+const wrapAdjective = (wrap = '*') => string =>
+  `You are ${wrap}${string}${wrap}!`;
+
+const Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+const actionApplyer = (startInt, arr) =>
+  arr.reduce((init, cb) => cb(init), startInt);
